dda2d19c6dd8b5753b88dacbcbbf1b26ed0cd573
Lucas Dantas <113190496+Lucasdantas03@users.noreply.github.com>
2023-04-03 22:40:49 +0200

    Linguagem C
    Linguagem C

--- Linguagem C Code Blocks/Problema Operadora.c
+++ Linguagem C Code Blocks/Problema Operadora.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Uma operadora de telefonia cobra R$ 50.00 por um plano b·sico que d· direito a 100 minutos de
+//Uma operadora de telefonia cobra R$ 50.00 por um plano b√°sico que d√° direito a 100 minutos de
 //telefone. Cada minuto que exceder a franquia de 100 minutos custa R$ 2.00. Fazer um programa para
-//ler a quantidade de minutos que uma pessoa consumiu, daÌ mostrar o valor a ser pago.
+//ler a quantidade de minutos que uma pessoa consumiu, da√≠ mostrar o valor a ser pago.
 //Data 30/03/2023
 
 int main(){
@@ -22,4 +22,5 @@ int main(){
     printf ("Valor a Pagar R$ %.2lf", ValorFinal);
 
     return 0;
+    
 }
