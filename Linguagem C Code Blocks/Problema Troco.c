9b643632211c4fe4bb5748cc51d43b84719aa7f1
Lucas Dantas <113190496+Lucasdantas03@users.noreply.github.com>
2023-04-27 21:37:06 +0200

    Estrutura sequencial

--- Linguagem C Code Blocks/Problema Troco.c
+++ Linguagem C Code Blocks/Problema Troco.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
 //Fazer um programa para calcular o troco no processo de pagamento de um produto de uma mercearia.
 //O programa deve ler o preço unitário do produto, a quantidade de unidades compradas deste produto,
 //e o valor em dinheiro dado pelo cliente (suponha que haja dinheiro suficiente). Seu programa deve
