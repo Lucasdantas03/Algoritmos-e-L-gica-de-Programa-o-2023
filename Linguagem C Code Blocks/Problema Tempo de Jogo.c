af0cc923f4033973ba9f6533d49c4584791842ac
Lucas Dantas <113190496+Lucasdantas03@users.noreply.github.com>
2023-04-27 21:45:36 +0200

    Estrutura condicional

--- Linguagem C Code Blocks/Problema Tempo de Jogo.c
+++ Linguagem C Code Blocks/Problema Tempo de Jogo.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 
 //Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo
-//pode começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24
-//horas.
+//pode começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.
 //Data : 31/03/2023
 
 int main(){
