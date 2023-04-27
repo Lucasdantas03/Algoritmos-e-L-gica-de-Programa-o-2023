d705b505d4bc7f662b0695363d7d9630c2176628
Lucas Dantas <113190496+Lucasdantas03@users.noreply.github.com>
2023-04-27 21:54:47 +0200

    Estruturas repetitivas (For)
    Estrutura Para

--- Linguagem C Code Blocks/Problema Media Ponderada.c
+++ Linguagem C Code Blocks/Problema Media Ponderada.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Leia um valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de
-//teste consiste de 3 valores reais, para os quais você deverá calcular e mostrar a média ponderada, sendo
+//Leia um valor inteiro N, que representa o nÃºmero de casos de teste que vem a seguir. Cada caso de
+//teste consiste de 3 valores reais, para os quais vocÃª deverÃ¡ calcular e mostrar a mÃ©dia ponderada, sendo
 //que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem peso 5. Vale lembrar
-//que a média ponderada é a soma de todos os valores multiplicados pelo seu respectivo peso, dividida
+//que a mÃ©dia ponderada Ã© a soma de todos os valores multiplicados pelo seu respectivo peso, dividida
 //pela soma dos pesos.
 //Data 27/04/2023
-
 
 int main(){
 
