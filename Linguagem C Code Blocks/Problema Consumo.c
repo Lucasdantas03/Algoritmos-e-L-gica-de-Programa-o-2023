30fe793e9e434f1c0032f9cbf3014abba5e7fce5
Lucas Dantas <113190496+Lucasdantas03@users.noreply.github.com>
2023-04-27 21:38:44 +0200

    Estrutura sequencial

--- Linguagem C Code Blocks/Problema Consumo.c
+++ Linguagem C Code Blocks/Problema Consumo.c
@@ -4,6 +4,7 @@
 //Fazer um programa para ler a distância total (em km) percorrida por um carro, bem como o total de
 //combustível gasto por este carro ao percorrer tal distância. Seu programa deve mostrar o consumo
 //médio do carro, com três casas decimais.
+
 //Data 28/03/2023
 
 int main(){
