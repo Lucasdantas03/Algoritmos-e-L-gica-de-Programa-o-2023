8fe6f425654fb11d134849fa9c0146e8af5b86c6
Lucas Dantas <113190496+Lucasdantas03@users.noreply.github.com>
2023-04-27 21:49:28 +0200

    Estruturas repetitivas (While)
    Estrutura Enquanto

--- Linguagem C Code Blocks/Problema Quadrante.c
+++ Linguagem C Code Blocks/Problema Quadrante.c
@@ -3,7 +3,7 @@
 
 //Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no
 //sistema cartesiano. Para cada ponto escrever o quadrante a que ele pertence (Q1, Q2, Q3 ou Q4). O
-//algoritmo será encerrado quando pelo menos uma de duas coordenadas for NULA (nesta situação sem
+//algoritmo serÃ¡ encerrado quando pelo menos uma de duas coordenadas for NULA (nesta situaÃ§Ã£o sem
 //escrever mensagem alguma).
 //Data 06/04/2023
 
@@ -14,7 +14,6 @@ int main(){
     printf ("Digite os valores das coordenadas X e Y: \n");
     scanf ("%lf", &x);
     scanf ("%lf", &y);
-
 
     while ( x != 0 && y !=0 ){
         if (x > 0 && y > 0)
