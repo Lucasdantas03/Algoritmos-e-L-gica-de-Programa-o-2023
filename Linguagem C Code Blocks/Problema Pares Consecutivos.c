8221d4cd1c0d5f78b7cd8d8c72842dc0e579a2ba
Lucas Dantas <113190496+Lucasdantas03@users.noreply.github.com>
2023-04-27 21:51:18 +0200

    Estruturas repetitivas (While)
    Estrutura Enquanto

--- Linguagem C Code Blocks/Problema Pares Consecutivos.c
+++ Linguagem C Code Blocks/Problema Pares Consecutivos.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//O programa deve ler um valor inteiro X indefinidas vezes. (O programa ir· parar quando o valor de X
+//O programa deve ler um valor inteiro X indefinidas vezes. (O programa ir√° parar quando o valor de X
 //for igual a 0). Para cada X lido, imprima a soma dos 5 pares consecutivos a partir de X, inclusive o X
-//, se for par. Se o valor de entrada for 4, por exemplo, a saÌda deve ser 40, que È o resultado da operaÁ„o:
-//4+6+8+10+12, enquanto que se o valor de entrada for 11, por exempo, a saÌda deve ser 80, que È a
+//, se for par. Se o valor de entrada for 4, por exemplo, a sa√≠da deve ser 40, que √© o resultado da opera√ß√£o:
+//4+6+8+10+12, enquanto que se o valor de entrada for 11, por exempo, a sa√≠da deve ser 80, que √© a
 //soma de 12+14+16+18+20.
 //Data 06/04/2023
-
 
 int main(){
 
