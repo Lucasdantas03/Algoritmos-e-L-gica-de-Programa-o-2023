761f1a1d7c3bc84207be4b45f714294ac7d52b8d
Lucas Dantas <113190496+Lucasdantas03@users.noreply.github.com>
2023-04-27 21:35:25 +0200

    Estrutura sequencial

--- Linguagem C Code Blocks/Problema Retangulo.c
+++ Linguagem C Code Blocks/Problema Retangulo.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <math.h>
 
-
 //Fazer um programa para ler as medidas da base e altura de um retângulo. Em seguida, mostrar o valor
 //da área, perímetro e diagonal deste retângulo, com quatro casas decimais, conforme exemplos.
 
