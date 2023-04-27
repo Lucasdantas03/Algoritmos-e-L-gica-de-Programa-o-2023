29823968d63a75125a4c422562dacfa3f6327cc4
Lucas Dantas <113190496+Lucasdantas03@users.noreply.github.com>
2023-04-27 21:53:50 +0200

    Estruturas repetitivas (For)
    Estrutura Para

--- Linguagem C Code Blocks/Problema Dentro Fora.c
+++ Linguagem C Code Blocks/Problema Dentro Fora.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Leia um valor inteiro N. Este valor ser· a quantidade de valores inteiros X que ser„o lidos em seguida.
-//Mostre quantos destes valores X est„o dentro do intervalo [10,20] e quantos est„o fora do intervalo,
+//Leia um valor inteiro N. Este valor ser√° a quantidade de valores inteiros X que ser√£o lidos em seguida.
+//Mostre quantos destes valores X est√£o dentro do intervalo [10,20] e quantos est√£o fora do intervalo,
 //conforme exemplo.
-
 //Data 27/04/2023
 
 int main(){
