f96b8099a2cc8f361836af280478c188ebf46386
Lucas Dantas <113190496+Lucasdantas03@users.noreply.github.com>
2023-04-27 22:03:40 +0200

    Matriz (Arrays)

--- Linguagem C Code Blocks/Problema Soma Linhas.c
+++ Linguagem C Code Blocks/Problema Soma Linhas.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Fazer um programa para ler dois números inteiros M e N (máximo = 10). Em seguida, ler uma matriz
-//de M linhas e N colunas contendo números reais. Gerar um vetor de modo que cada elemento do vetor
+//Fazer um programa para ler dois nÃºmeros inteiros M e N (mÃ¡ximo = 10). Em seguida, ler uma matriz
+//de M linhas e N colunas contendo nÃºmeros reais. Gerar um vetor de modo que cada elemento do vetor
 //seja a soma dos elementos da linha correspondente da matriz. Mostrar o vetor gerado
 //Data 27/04/2023
-
 
 int main(){
 
@@ -38,7 +37,6 @@ int main(){
     for (int i = 0; i < M; i++){
         printf ("%.1lf\n", vet[i]);
     }
-
 
     return 0;
 }
